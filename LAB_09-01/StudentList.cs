8a7b0e66c401ed94c083de1f43e65d0ffc766d63
BShark1985 <leonand1985@gmail.com>
2022-07-17 21:16:38 +0300

    LABS 04 and 09

---
+++ LAB_09-01/StudentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace LAB_09_01
+{
+    public class StudentList : ObservableCollection<Student>
+    {
+
+    }
+}
